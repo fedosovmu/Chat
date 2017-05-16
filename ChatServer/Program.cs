25163e1d261d7ebc020aca8838ab592d192c5efc
fedosovmu <fedosovmu@gmail.com>
2017-05-16 20:55:12 +0500

    Добавьте файлы проекта.

---
+++ ChatServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Программа запущена");
+
+			IPHostEntry host1 = Dns.GetHostEntry("google.ru");
+			Console.WriteLine(host1.HostName);
+			foreach (IPAddress ip in host1.AddressList)
+				Console.WriteLine(ip.ToString());
+			
+			Console.ReadKey();
+		}
+	}
+}
